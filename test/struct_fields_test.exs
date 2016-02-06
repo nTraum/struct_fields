c23889050191a844c615f0b1063de62f7f7c4424
Matt Ahrens <matthew.ahrens@tufts.edu>
2016-02-06 17:44:20 -0500

    used the before_compile convention so that __MODULE__ would be populated as a struct even if use was put before destruct

--- test/struct_fields_test.exs
+++ test/struct_fields_test.exs
@@ -12,4 +12,16 @@ defmodule StructFieldsTest do
     assert Enum.member?(MyModule.fields, :foo)
     assert Enum.member?(MyModule.fields, :bar)
   end
+
+  test "use declared anywhere at top level" do
+    defmodule MyModule do
+      use StructFields
+
+      defstruct greeting: :hello
+      def greet(%MyModule{greeting: g}), do: g
+    end
+
+    assert length(MyModule.fields) == 1
+    assert Enum.member?(MyModule.fields, :greeting)
+  end
 end
